8eb053346182717ab0384954b55772e87e1b67e2
jaon <>
2021-03-22 11:57:52 +0800

    feat: MINE-1154 home page content

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,74 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        nuxt-web-desgin
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <base-container class="banner-container">
+    <div class="banner-content">
+      <div>
+        <div class="banner-text">
+          <div> Minprofit 盈算力</div>
+          <div>全球顶尖云算力平台</div>
+        </div>
+        <a-button type="primary" shape="round" class="download-button" size="large">立即下载</a-button>
       </div>
+
+      <img :src="bannerPhoneImage" class="banner-phone" alt="">
     </div>
-  </div>
+  </base-container>
 </template>
 
 <script>
+import { Button } from 'ant-design-vue';
+import BaseContainer from '@/shared/components/base-container';
+import bannerPhoneImage from '@/assets/banner-phone.png';
 
 export default {
+  components: {
+    'a-button': Button,
+    'base-container': BaseContainer,
+  },
 
+  data() {
+    return {
+      bannerPhoneImage,
+    };
+  },
 };
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="less">
+@import './index.less';
 </style>
---
+++ src/shared/intl/messages/home.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+export default {
+  "abnormalSettlement": "异常结算 {msg}",
+  "check": "查看",
+  "clear": "清除",
+}
