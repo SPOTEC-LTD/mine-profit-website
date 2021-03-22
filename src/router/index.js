f4c4aac4e2e79d8a012fadfb87b8970bd3e9f4ec
VeigarChen-Spotec <veigar.c@spotec.net>
2021-03-22 10:55:11 +0800

    feat: MINE-1167 official website help page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/home';
 import Login from '@/pages/login';
+import Help from '@/pages/help';
 
 Vue.use(Router);
 
@@ -16,6 +17,10 @@ export function createRouter() {
       {
         path: '/login',
         component: Login,
+      },
+      {
+        path: '/help',
+        component: Help,
       },
     ],
   });
