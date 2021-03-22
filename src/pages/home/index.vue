cecfdc9bc9cc6db12b75847443f7b1261264140a
jaon <>
2021-03-22 17:06:09 +0800

    feat: MINE-1154-home-page banner and summary

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,28 +1,42 @@
 <template>
-  <base-container class="banner-container">
-    <div class="banner-content">
-      <div>
-        <div class="banner-text">
-          <div> Minprofit 盈算力</div>
-          <div>全球顶尖云算力平台</div>
+  <div>
+    <base-container class="banner-container">
+      <div class="banner-content">
+        <div>
+          <div class="banner-text">
+            <div> Minprofit 盈算力</div>
+            <div>全球顶尖云算力平台</div>
+          </div>
+          <a-button type="primary" shape="round" class="download-button" size="large">
+            <h-qrcode class-name="qr-code" />
+            <span>
+              立即下载
+            </span>
+          </a-button>
         </div>
-        <a-button type="primary" shape="round" class="download-button" size="large">立即下载</a-button>
-      </div>
 
-      <img :src="bannerPhoneImage" class="banner-phone" alt="">
-    </div>
-  </base-container>
+        <img :src="bannerPhoneImage" class="banner-phone" alt="">
+      </div>
+    </base-container>
+    <base-container>
+      <subject-summary />
+    </base-container>
+  </div>
 </template>
 
 <script>
 import { Button } from 'ant-design-vue';
+import QrCode from 'ahoney/lib/icons/QrCode';
 import BaseContainer from '@/shared/components/base-container';
 import bannerPhoneImage from '@/assets/banner-phone.png';
+import SubjectSummary from './subject-summary.vue';
 
 export default {
   components: {
     'a-button': Button,
+    'h-qrcode': QrCode,
     'base-container': BaseContainer,
+    'subject-summary': SubjectSummary,
   },
 
   data() {
---
+++ src/pages/home/subject-summary.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-container>
+    <div>
+      <div class="subject-summary-container">
+        <square-dots-icon class="summary-dots-icon" />
+        <div class="subject-summary-img">
+          <img src="@/assets/summary-img.png" alt="">
+        </div>
+        <div>
+          <div class="subject-summary-content">
+            <block-title :img="aboutTitleImage" class="about-title-image" title="盈算力" />
+            是由注册于塞舌尔 Seychelles 的国际加密资产投资管理公司 Mineprofit 投资的去中心化算力投资平台，拥有塞舌尔金融牌照，受到塞舌尔金融服务管理局监管，保障广大投资者利益。
+          </div>
+        </div>
+      </div>
+    </div>
+  </base-container>
+</template>
+
+<script>
+
+import BaseContainer from '@/shared/components/base-container';
+import bannerPhoneImage from '@/assets/banner-phone.png';
+import aboutTitleImage from '@/assets/about-title.png';
+import BlockTitle from './block-title';
+import SquareDotsIcon from './square-dots-icon';
+
+export default {
+  components: {
+    'base-container': BaseContainer,
+    'block-title': BlockTitle,
+    'square-dots-icon': SquareDotsIcon,
+  },
+
+  data() {
+    return {
+      bannerPhoneImage,
+      aboutTitleImage,
+    };
+  },
+};
+</script>
