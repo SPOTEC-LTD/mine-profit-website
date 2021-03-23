d7da1c4e4145274a657d799c35146f20e7890e19
MorganQin-Spotec <morganqin-spotec@outlook.com>
2021-03-23 13:52:53 +0800

    feat: MINE-1153 display global page footer

--- src/shared/components/page-footer/index.vue
+++ src/shared/components/page-footer/index.vue
@@ -1,12 +1,55 @@
 <template>
-  <div>
-    这是footer
+  <div class="page-footer">
+    <div class="page-footer-container">
+      <div class="page-footer-subject">
+        <img class="page-footer-vision" src="@/assets/footer-subject.png" alt="" />
+        <div class="page-footer-powered">
+          Powered by
+        </div>
+        <div class="page-footer-project">
+          Mineprofit
+        </div>
+      </div>
+      <div class="page-footer-navs">
+        <div>
+          <div class="page-footer-nav-category">
+            信息
+          </div>
+          <a class="page-footer-nav-item">
+            快讯
+          </a>
+          <a class="page-footer-nav-item">
+            好文
+          </a>
+          <a class="page-footer-nav-item">
+            公告
+          </a>
+        </div>
+        <div>
+          <div class="page-footer-nav-category">
+            支持
+          </div>
+          <a class="page-footer-nav-item">
+            帮助中心
+          </a>
+        </div>
+        <div>
+          <div class="page-footer-nav-category">
+            商务合作
+          </div>
+          <a class="page-footer-nav-item">
+            联系我们
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-export default {
-
-};
+export default {};
 </script>
+
+<style lang="less">
+@import "./index.less";
+</style>
