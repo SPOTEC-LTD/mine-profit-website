1a09f8e2d5a008ac63e395a1df8b767a951c1c84
MorganQin-Spotec <morganqin-spotec@outlook.com>
2021-03-25 14:16:14 +0800

    feat: MINE-1241 500 page

--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  "searchInputPlaceholder": "搜索您想问的问题2",
+  "searchInputPlaceholder": "搜索您想问的问题",
   "search": "搜索",
   "commonProblem": "常见问题",
   "searchPrompt": "关于“{searchValue}”的搜索结果共有",
@@ -13,5 +13,6 @@ export default {
   "announcement": "公告",
   "allContentLoaded": "已加载全部内容",
   "sorryPageNotFound": "抱歉，您访问的页面不存在",
-  "backHome": "返回首页"
+  "backHome": "返回首页",
+  "sorryPageMeetsError": "抱歉，您访问的页面有误"
 }
--- src/shared/intl/messages/common.js
+++ src/shared/intl/messages/common.js
@@ -2,4 +2,5 @@
 export default {
   "sorryPageNotFound": "抱歉，您访问的页面不存在",
   "backHome": "返回首页",
+  "sorryPageMeetsError": "抱歉，您访问的页面有误",
 }
