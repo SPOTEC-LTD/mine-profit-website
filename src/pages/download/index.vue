e209246313e8fac4d190c59557a68571549fe67b
MorganQin-Spotec <morganqin-spotec@outlook.com>
2021-03-31 16:00:30 +0800

    feat: MINE-1355 update styles of scenario 1 & 2 to fix qrcode section

--- src/pages/download/index.vue
+++ src/pages/download/index.vue
@@ -6,7 +6,12 @@
         <img class="img-2" src="@/assets/download/1-2.png" alt="">
         <img class="img-3" src="@/assets/download/1-3.png" alt="">
       </div>
-      <div class="content">
+      <div v-if="scenarioIndex === 1" class="main-imgs">
+        <img class="img-1" src="@/assets/download/2-1.png" alt="">
+        <img class="img-2" src="@/assets/download/2-2.png" alt="">
+        <img class="img-3" src="@/assets/download/2-3.png" alt="">
+      </div>
+      <div class="main-content">
         <div v-if="scenarioIndex === 0">
           <div class="text-content">
             <img src="@/assets/download/project.png" alt="">
@@ -35,10 +40,44 @@
             </div>
           </div>
           <div class="glass-1 glass w-172" />
-          <div class="blue-1 lake-blue w-72" />
-          <div class="square-1 pomegranate-outlined-square w-90" />
-          <div class="blue-2 lake-blue w-90" />
+          <div class="square-1 lake-blue w-72" />
+          <div class="square-o-1 pomegranate-outlined-square w-90" />
+          <div class="square-2 lake-blue w-90" />
           <square-dots-icon class="matrix-1 dot-matrix" />
+        </div>
+        <div v-if="scenarioIndex === 1">
+          <div class="text-content">
+            <div class="title">
+              算力生态圈，助你获利非凡
+            </div>
+            <div class="description">
+              完善的算力转让、质押生态圈，让你实现灵活的算力资产配置，获得最优收益。
+            </div>
+          </div>
+          <div class="qrcode-container">
+            <div class="download-now">
+              立即扫码下载
+            </div>
+            <div class="qrcode">
+              <qrcode value="www.google.com" />
+            </div>
+            <div class="version-ios">
+              <img src="@/assets/download/apple.png" alt="">
+              <div>
+                版本：1.0.1
+              </div>
+            </div>
+            <div class="version-android">
+              <img src="@/assets/download/android.png" alt="">
+              <div>
+                版本：1.0.1
+              </div>
+            </div>
+          </div>
+          <div class="square-o-1 pomegranate-outlined-square w-120" />
+          <div class="square-1 lake-blue w-90" />
+          <square-dots-icon class="matrix-1 dot-matrix" />
+          <div class="line-1 lake-blue l4-90" />
         </div>
       </div>
     </div>
@@ -64,7 +103,7 @@ export default {
   },
   data() {
     return {
-      scenarioIndex: 0,
+      scenarioIndex: 1,
     };
   },
 };
