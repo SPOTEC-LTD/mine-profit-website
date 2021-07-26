2551c6a2d2a780d564e98e270d05d7ba3fdd2f35
Jason <>
2021-07-26 18:39:21 +0800

    feat: modify base container props

--- src/shared/components/BaseContainer/index.jsx
+++ src/shared/components/BaseContainer/index.jsx
@@ -1,11 +1,13 @@
+import './index.less';
+
 const BaseContainer = {
   props: ['contentClassName'],
 
   render() {
     return (
       <div class="container">
-        <div class="content">
-          <slot />
+        <div class={['content', this.contentClassName]}>
+         {this.$slots.default}
         </div>
       </div>
     );
