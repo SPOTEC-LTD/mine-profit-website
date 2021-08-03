32a50bd2223b49e20b71e161d5cfedbfbca2c262
EllaLee-spotec <75291282+EllaLee-spotec@users.noreply.github.com>
2021-08-03 10:19:39 +0800

    feat: Ella-MinePc-10 c2c product details (#130)

--- src/pages/ProductMarketing/components/CellGroup/index.jsx
+++ src/pages/ProductMarketing/components/CellGroup/index.jsx
@@ -6,18 +6,24 @@ const CellGroup = {
   props: {
     cellData: { type: Array },
     className: { type: String },
+    isC2CMarket: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   render() {
+    const colSpan = this.isC2CMarket ? 6 : 4.8;
     return (
       <div class={['cell-group-wrapper', this.className]}>
         <Row gutter={20} type="flex">
           {this.cellData.map((item, index) => (
-            <Col span={4.8}>
+            <Col span={colSpan}>
               <SingleCell
                 class='cell-single-list'
                 key={index}
                 className={item.cellClass}
+                isC2CMarket={this.isC2CMarket}
                 scopedSlots={{
                   title: () => item.title,
                   icon: () => item.icon,
