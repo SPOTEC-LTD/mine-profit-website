87c2f532100381f2a78c955c4785384ee621e96a
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-08-03 15:37:20 +0800

    feat: number-input component (#134)

---
+++ src/shared/utils/filterFloat.js
@@ -0,0 +1,23 @@
+const filterFloat = ({ value, preValue, precision, enableMinus }) => {
+  let nextValue = value.replace(/[^\-\d.]/g, '');
+  const matchDecimalPoint = nextValue.match(/[.]/g) || [];
+  if (matchDecimalPoint.length > 1) {
+    return preValue;
+  }
+
+  if (!enableMinus) {
+    nextValue = value.replace(/[^0-9.]/g, '');
+  }
+
+  const floatNumberReg = /^\-?\d+\.?\d*$/;
+
+  if (nextValue && nextValue.length > 1 && !floatNumberReg.test(nextValue)) {
+    return preValue;
+  }
+
+  const twoDecimalNumReg = new RegExp(`^(\\-)*(\\d+)\\.(\\d{${precision}}).*$`);
+
+  return nextValue.replace(twoDecimalNumReg, '$1$2.$3');
+};
+
+export default filterFloat;
