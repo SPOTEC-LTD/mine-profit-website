58d9e8f836b1197c4c9205d2da6737da54171286
TinnerLee-Spotec <84693560+TinnerLee-Spotec@users.noreply.github.com>
2021-08-16 17:54:29 +0800

    feat: banner rebuild (#159)

--- src/pages/home/Banner/index.jsx
+++ src/pages/home/Banner/index.jsx
@@ -1,39 +1,13 @@
 import { Carousel } from 'ant-design-vue';
-import cnHomeBanner1 from '@/assets/home/home-banner-1.png';
-import cnHomeBanner2 from '@/assets/home/home-banner-2.png';
-import cnHomeBanner3 from '@/assets/home/home-banner-3.png';
-import enHomeBanner1 from '@/assets/home/en-home-banner-1.png';
-import enHomeBanner2 from '@/assets/home/en-home-banner-2.png';
-import enHomeBanner3 from '@/assets/home/en-home-banner-3.png';
-import { getIsChinese } from '@/shared/utils/getLocalLanguage';
-
 import styles from './index.less?module';
 
-const bannerMap = {
-  cnBannerImgList: [{
-    id: 1, src: cnHomeBanner1,
-  },
-  {
-    id: 2, src: cnHomeBanner2,
-  },
-  {
-    id: 3, src: cnHomeBanner3,
-  }],
-  enBannerImgList: [{
-    id: 1, src: enHomeBanner1,
-  },
-  {
-    id: 2, src: enHomeBanner2,
-  },
-  {
-    id: 3, src: enHomeBanner3,
-  }],
-};
-
 const Home = {
-  computed: {
-    finallyBannerImgList() {
-      return getIsChinese() ? bannerMap.cnBannerImgList : bannerMap.enBannerImgList;
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return [];
+      },
     },
   },
 
@@ -42,13 +16,14 @@ const Home = {
       <div class={styles['banner-box']}>
         <Carousel autoplay>
           {
-            this.finallyBannerImgList.map(({ id, src }) => (
-              <img src={src} alt="" key={id} />
+            this.list.map(({ id, webImage }) => (
+              <div class={styles['banner-img-box']}>
+                <img src={webImage} alt="" key={id} />
+              </div>
             ))
           }
         </Carousel>
       </div>
-
     );
   },
 };
