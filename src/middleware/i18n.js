0812b619dd988ae7225d87de9121f3fb7ce19e7b
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-08-18 11:46:26 +0800

    feat: MINEPC-59 first i18n init (#169)

--- src/middleware/i18n.js
+++ src/middleware/i18n.js
@@ -1,17 +1,29 @@
+/* eslint-disable consistent-return */
 import Cookies from 'universal-cookie';
 import startsWith from 'lodash/startsWith';
 
 export default function (ctx) {
   if (ctx.isHMR) { return; }
 
-  const { defaultLocale } = ctx.app.i18n;
+  const { defaultLocale, locale } = ctx.app.i18n;
+  const header = (ctx.req && ctx.req.headers) || {};
 
-  const header = ctx.req && ctx.req.headers && ctx.req.headers.cookie;
-  const { language } = new Cookies(header).cookies;
+  const headerCookie = header.cookie;
+  const { language } = new Cookies(headerCookie).cookies;
+  if (!language && header['accept-language']) {
+    const [clientLang] = header['accept-language'].split(',');
+
+    if (clientLang.toLowerCase().indexOf('zh') === -1) {
+      if (locale !== 'en') {
+        return ctx.redirect(`/en${ctx.route.fullPath}`);
+      }
+    } else if (locale !== 'zh') {
+      return ctx.redirect(ctx.route.fullPath.replace(/^\/en/, ''));
+    }
+  }
 
   if (language && language !== defaultLocale) {
     if (!startsWith(ctx.route.fullPath, `/${language}`)) {
-      // eslint-disable-next-line consistent-return
       return ctx.redirect(
         `/${language}${ctx.route.fullPath}`,
       );
