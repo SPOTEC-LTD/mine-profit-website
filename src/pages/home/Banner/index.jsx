62448e1c76a492c48859ef9586b1ddd9017ba56e
TinnerLee-Spotec <84693560+TinnerLee-Spotec@users.noreply.github.com>
2021-08-25 18:32:09 +0800

    feat: banner detail (#190)

--- src/pages/home/Banner/index.jsx
+++ src/pages/home/Banner/index.jsx
@@ -1,4 +1,10 @@
 import { Carousel } from 'ant-design-vue';
+import { bannerDetailPath } from '@/router/consts/urls';
+import getUserInfoFunc from '@/shared/utils/request/getUserInfoFunc';
+import locationHelp from '@/shared/utils/locationHelp';
+
+import { LINK, H5_CONTENT } from './bannerTypes';
+
 import styles from './index.less?module';
 
 const Home = {
@@ -11,14 +17,29 @@ const Home = {
     },
   },
 
+  methods: {
+    viewDetail(banner) {
+      const { linkType, linkUrl, id } = banner;
+      if (linkType === LINK) {
+        window.open(linkUrl);
+      } else if (linkType === H5_CONTENT) {
+        const { userId = 'null' } = getUserInfoFunc();
+        locationHelp.open(bannerDetailPath, { params: { userId, id } });
+      }
+    },
+  },
+
   render() {
     return (
       <div class={styles['banner-box']}>
         <Carousel autoplay>
           {
-            this.list.map(({ id, webImage }) => (
-              <div class={styles['banner-img-box']}>
-                <img src={webImage} alt="" key={id} />
+            this.list.map(item => (
+              <div
+                class={styles['banner-img-box']}
+                onClick={() => { this.viewDetail(item); }}
+              >
+                <img src={item.webImage} alt="" key={item.id} />
               </div>
             ))
           }
