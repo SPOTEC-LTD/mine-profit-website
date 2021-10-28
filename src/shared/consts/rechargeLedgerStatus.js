747ade5786c5cd61f204c1ac2ca10ab3bae79fb5
Tinner <tinner.lee@spotec.net>
2021-10-28 15:46:42 +0800

    fix: MINE-3700 修复钱包明细中无回购展示bug

--- src/shared/consts/rechargeLedgerStatus.js
+++ src/shared/consts/rechargeLedgerStatus.js
@@ -1,12 +1,15 @@
 import locale from '@/shared/intl/utils/locale';
 
-export const WALLET_STATUS_ALL = '';
+/** 所有类型 */
+// export const WALLET_TYPE_ALL = '';
+/** 所有状态，所有类型 */
+export const ALL = '';
 export const PROGRESSING = 1;
 export const FAIL = 2;
 export const SUCCESS = 3;
 
 export const getLedgerStatusList = () => ([
-  { text: locale.t('walletStatusAll'), value: WALLET_STATUS_ALL },
+  { text: locale.t('walletStatusAll'), value: ALL },
   { text: locale.t('walletStatusOngoing'), value: PROGRESSING },
   { text: locale.t('walletStatusFailure'), value: FAIL },
   { text: locale.t('walletStatusSuccess'), value: SUCCESS },
