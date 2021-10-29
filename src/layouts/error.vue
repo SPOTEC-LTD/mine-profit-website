61bd376907758ab034c98f8571d1d1304c9faf99
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-10-29 11:43:25 +0800

    fix: 500 page redirect (#257)

---
+++ src/layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Page404 v-if="error.statusCode === 404" />
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+import Page404 from '@/pages/404';
+
+export default {
+  components: {
+    Page404,
+  },
+
+  props: ['error'],
+  layout: 'default',
+  mounted() {
+    console.log('error', this.error);
+  },
+};
+</script>
