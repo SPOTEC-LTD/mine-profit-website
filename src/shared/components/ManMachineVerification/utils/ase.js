7cec48e1b22eefbb14a23fa12c72711650007de5
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-11-05 14:01:19 +0800

    fix: ie11 js bug (#280)

--- src/shared/components/ManMachineVerification/utils/ase.js
+++ src/shared/components/ManMachineVerification/utils/ase.js
@@ -1,11 +1,26 @@
-import CryptoJS from 'crypto-js';
+import JsCrypto from 'jsCrypto';
 /**
  * @word 要加密的内容
  * @keyWord String  服务器随机返回的关键字
  *  */
+/**
+ * crypto-js ie11 兼容性有问题使用jscrypto替代
+ * https://www.npmjs.com/search?q=jscrypto
+ * https://github.com/Hinaser/jscrypto/blob/master/API.md#hmac-sha384
+ *
+ */
+// export function aesEncrypt(word, keyWord = 'XwKsGlMcdPMEhR1B') {
+//   const key = CryptoJS.enc.Utf8.parse(keyWord);
+//   console.log('key', key);
+
+//   const srcs = CryptoJS.enc.Utf8.parse(word);
+//   const encrypted = CryptoJS.AES.encrypt(srcs, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
+//   return encrypted.toString();
+// }
+
 export function aesEncrypt(word, keyWord = 'XwKsGlMcdPMEhR1B') {
-  const key = CryptoJS.enc.Utf8.parse(keyWord);
-  const srcs = CryptoJS.enc.Utf8.parse(word);
-  const encrypted = CryptoJS.AES.encrypt(srcs, key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
+  const key = JsCrypto.Utf8.parse(keyWord);
+  const srcs = JsCrypto.Utf8.parse(word);
+  const encrypted = JsCrypto.AES.encrypt(srcs, key, { mode: JsCrypto.mode.ECB, padding: JsCrypto.pad.Pkcs7 });
   return encrypted.toString();
 }
