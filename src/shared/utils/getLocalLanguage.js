d97613020dd7fecfed8750a65003fcb102bdcf8e
VeigarChen-Spotec <veigar.c@spotec.net>
2021-11-17 09:53:04 +0800

    fix: MINE-4007 i18n link problem

--- src/shared/utils/getLocalLanguage.js
+++ src/shared/utils/getLocalLanguage.js
@@ -1,13 +1,9 @@
-import { EN, ZH } from '@@/i18n';
+import { EN, ZH, languages } from '@@/i18n';
 import locale from '@/shared/intl/utils/locale';
 
 export const getIsEnglish = () => locale.currentLocale === EN;
 export const getIsChinese = () => locale.currentLocale === ZH;
 
-export const getLocalLanguage = () => {
-  const isEnglish = getIsEnglish();
-  const localLanguage = isEnglish ? EN : ZH;
-  return localLanguage;
-};
+export const getLocalLanguage = () => languages[locale.currentLocale];
 
 export default getLocalLanguage;
