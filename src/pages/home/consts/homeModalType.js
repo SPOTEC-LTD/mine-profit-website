140cf7bc7bf700f2301139cc191ebd299a669c13
VeigarChen-Spotec <72638372+VeigarChen-Spotec@users.noreply.github.com>
2021-11-23 09:47:57 +0800

    feat: home modal enhancement (#356)

---
+++ src/pages/home/consts/homeModalType.js
@@ -0,0 +1,5 @@
+export const HASHRATE_MODAL = 1;
+export const HASHRATE_COUPON_MODAL = 2;
+export const BANNER_MODAL = 3;
+export const ACTIVITY_MODAL = 4;
+export const WEEKLY_OUTPUT_REPORT_MODAL = 5;
